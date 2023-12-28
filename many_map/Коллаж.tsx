b6f693b5794e791ba98c41331d3a5be79aa55896
KochetyginMihail <149471714+KochetyginMihail@users.noreply.github.com>
2023-12-28 18:16:45 +0300

    Add files via upload
    Попытка загрузить папку

---
+++ many_map/Коллаж.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Коллаж" tilewidth="100" tileheight="100" tilecount="25" columns="5">
+ <image source="Коллаж.jpg" width="500" height="500"/>
+</tileset>
