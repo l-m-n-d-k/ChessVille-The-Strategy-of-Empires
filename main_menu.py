9f1781843537582b6664c14ddc48fb344361621f
Dokukin Dmitry <72608150+l-m-n-d-k@users.noreply.github.com>
2024-01-07 06:03:53 +0300

    main-menu

--- main_menu.py
+++ main_menu.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import sys
 import pygame
 import os
@@ -40,4 +41,48 @@ def main_menu(screen, fps, clock):
                     sys.exit()
 
         pygame.display.update()
+=======
+import sys
+import pygame
+import os
+from classes_map import Map
+from classes_camera_cursor_pause_timer import MyCursor, Camera
+from classes_icons_and_select import PlayerIcon
+from classes_info import MiniMap
+from groups_sprites import all_sprites, tiles_group, players_group1, players_group2, neytral_group, tyman_group1, \
+    tyman_group2, system_group
+from constants import *
+
+def main_menu(screen, fps, clock):
+    background_image = pygame.image.load('menu/fon.png')
+    background_image = pygame.transform.scale(background_image, (screen.get_width(), screen.get_height()))
+    menu_running = True
+    while menu_running:
+        screen.blit(background_image, (0, 0))
+        font = pygame.font.Font(None, 36)
+
+        # Создание текстовых объектов для кнопок
+        start_text = font.render('Начать игру', True, (255, 255, 255))
+        exit_text = font.render('Выход', True, (255, 255, 255))
+
+        # Координаты кнопок
+        start_button = start_text.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2 - 50))
+        exit_button = exit_text.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2 + 50))
+
+        screen.blit(start_text, start_button)
+        screen.blit(exit_text, exit_button)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                menu_running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if start_button.collidepoint(event.pos):
+                    pygame.mouse.set_visible(False)
+                    menu_running = False  # Завершение меню и начало игры
+                elif exit_button.collidepoint(event.pos):
+                    pygame.quit()  # Выход из игры
+                    sys.exit()
+
+        pygame.display.update()
+>>>>>>> Stashed changes
         clock.tick(fps)
