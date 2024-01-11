e9a8815e33b04e9474db17659c15f07190d573a2
KochetyginMihail <149471714+KochetyginMihail@users.noreply.github.com>
2024-01-11 07:49:37 +0300

    Комментарии к коду

--- groups_sprites.py
+++ groups_sprites.py
@@ -1,13 +1,13 @@
 import pygame
 
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-players_group1 = pygame.sprite.Group()
-players_group2 = pygame.sprite.Group()
-neytral_group = pygame.sprite.Group()
-tyman_group1 = pygame.sprite.Group()
-tyman_group2 = pygame.sprite.Group()
-info_group = pygame.sprite.Group()
-system_group = pygame.sprite.Group()
-button_group = pygame.sprite.Group()
-window_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()  # группа всех спрайтов
+tiles_group = pygame.sprite.Group()  # поле, холм, река, гора
+players_group1 = pygame.sprite.Group()  # герои первого игрока
+players_group2 = pygame.sprite.Group()  # герои второго игрока
+neytral_group = pygame.sprite.Group()  # нейтральные юниты
+tyman_group1 = pygame.sprite.Group()  # туман войны от лица первого игрока
+tyman_group2 = pygame.sprite.Group()  # туман войны от лица второго игрока
+info_group = pygame.sprite.Group()  # миникарта, табличка с очками передвижения и боевой мощью,
+button_group = pygame.sprite.Group()  # 2 круглых кнопки на миникарте, кнопка следующего хода
+window_group = pygame.sprite.Group()  # для всплывающих окошек (таких пока не было)
+system_group = pygame.sprite.Group()  # иконки и курсор
