97c79f15aa950a6272f45dc892f15cebb4a5912d
l-m-n-d-k <dima2642007@gmail.com>
2024-01-23 13:45:35 +0300

    update music

--- classes_music.py
+++ classes_music.py
@@ -5,14 +5,16 @@ class Music():
          pygame.mixer.init()
 
     def in_game(self):
+        pygame.mixer.music.stop()
         pygame.mixer.music.load('music/Pirate Ship (Acoustic).mp3')
-        pygame.mixer.music.set_volume(0.3)
+        pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(-1)
 
     def in_menu(self):
+        pygame.mixer.music.stop()
         pygame.mixer.music.load('music/Pirate Bay.mp3')
         pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(-1)
-    
+     
     def in_credits(self):
         ...
